52522737e0e4a3d2ac39e6f9e2ce53a6c37b453c
mrrobot <bulavinoff.vladislav@yandex.ru>
2024-08-20 19:35:13 +0300

    done

---
+++ README.md
@@ -0,0 +1,26 @@
+# REST API Для Создания TODO Списков на Go
+
+## <a href="https://www.youtube.com/playlist?list=PLbTTxxr-hMmyFAvyn7DeOgNRN8BQdjFm8">Видеокурс на YouTube</a>
+
+## В курсе разобранны следующие концепции:
+- Разработка Веб-Приложений на Go, следуя дизайну REST API.
+- Работа с фреймворком <a href="https://github.com/gin-gonic/gin">gin-gonic/gin</a>.
+- Подход Чистой Архитектуры в построении структуры приложения. Техника внедрения зависимости.
+- Работа с БД Postgres. Запуск из Docker. Генерация файлов миграций. 
+- Конфигурация приложения с помощью библиотеки <a href="https://github.com/spf13/viper">spf13/viper</a>. Работа с переменными окружения.
+- Работа с БД, используя библиотеку <a href="https://github.com/jmoiron/sqlx">sqlx</a>.
+- Регистрация и аутентификация. Работа с JWT. Middleware.
+- Написание SQL запросов.
+- Graceful Shutdown
+
+### Для запуска приложения:
+
+```
+make build && make run
+```
+
+Если приложение запускается впервые, необходимо применить миграции к базе данных:
+
+```
+make migrate
+```
---
+++ pkg/handler/handler.go
@@ -0,0 +1,58 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zhashkevych/todo-app/pkg/service"
+
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	_ "github.com/zhashkevych/todo-app/docs"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api", h.userIdentity)
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+			}
+		}
+
+		items := api.Group("items")
+		{
+			items.GET("/:id", h.getItemById)
+			items.PUT("/:id", h.updateItem)
+			items.DELETE("/:id", h.deleteItem)
+		}
+	}
+
+	return router
+}
